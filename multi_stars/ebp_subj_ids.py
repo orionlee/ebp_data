d312a936fbd0bc97ec81ac4dd065857a3efe62a2
orionlee <orionlee@yahoo.com>
2025-04-02 16:34:30 -0700

    Multi Stars: combine subject ids from all sources

--- multi_stars/ebp_subj_ids.py
+++ multi_stars/ebp_subj_ids.py
@@ -10,15 +10,19 @@ from zn_common import get_subject_ids_of_tag, get_subject_ids_of_collection
 EBP_ZN_ID = 22939
 
 
-def save_subject_ids_of_page(out_path, subject_ids, call_i, call_kwargs):
+def save_subject_ids_of_page(out_path, subject_ids, call_i=None, call_kwargs=None):
     with open(out_path, "a") as f:
         np.savetxt(f, subject_ids, fmt="%s")
 
     return out_path
 
 
+def _csv_path_of_tag(tag):
+    return Path(f"data/cache/ebp_tag_subj_ids_{tag}.csv")
+
+
 def save_all_subject_ids_of_tag(tag, skip_if_exists=True):
-    out_path = Path(f"data/cache/ebp_tag_subj_ids_{tag}.csv")
+    out_path = _csv_path_of_tag(tag)
 
     if skip_if_exists and out_path.exists() and out_path.stat().st_size > 0:
         print(f"[DEBUG] tag {tag} skipped (with existing data)")
@@ -44,9 +48,13 @@ def save_all_subject_ids_from_tags(tags=MULTI_STAR_TAGS, skip_if_exists=True):
         save_all_subject_ids_of_tag(a_tag, skip_if_exists=skip_if_exists)
 
 
-def save_all_subject_ids_of_collection(collection_name, skip_if_exists=True):
+def _csv_path_of_collection(collection_name):
     collection_name_encoded = re.sub("[/\:]", "_", collection_name)
-    out_path = Path(f"data/cache/ebp_coll_subj_ids_{collection_name_encoded}.csv")
+    return Path(f"data/cache/ebp_coll_subj_ids_{collection_name_encoded}.csv")
+
+
+def save_all_subject_ids_of_collection(collection_name, skip_if_exists=True):
+    out_path = _csv_path_of_collection(collection_name)
 
     if skip_if_exists and out_path.exists() and out_path.stat().st_size > 0:
         print(f"[DEBUG] collection {collection_name} skipped (with existing data)")
@@ -73,27 +81,31 @@ def save_all_subject_ids_from_collections(collections=MULTI_STAR_COLLECTIONS, sk
         save_all_subject_ids_of_collection(a_coll, skip_if_exists=skip_if_exists)
 
 
-# #
-# # Top level driver
-# #
-# if __name__ == "__main__":
-#     # params for sectors #eclipsingbinary in sectors 1 to 39
-#     # Subject 68601250 is the first sector 40 on page 3230
-#     # get_subject_ids_of_tag(
-#     #     "eclipsingbinary", 1, 3230, end_subject_id_exclusive=68601250, page_result_func=save_subject_ids_of_page
-#     # )
+def _combine_n_save_all_subject_ids():
+    out_path = Path("data/ebp_subj_ids.csv")
 
-#     # I decided to crawl all pages instead of trying to limit to sectors 1 to 39
-#     # because the subject ids are not strictly increasing.
-#     # Filtering will be done afterwards
-#     page_start, page_end_inclusive, end_subject_id_exclusive = 1, 3602, None
-#     # page_start, page_end_inclusive, end_subject_id_exclusive = 1001, 3602, None
+    paths_tags = [_csv_path_of_tag(t) for t in MULTI_STAR_TAGS]
+    paths_colls = [_csv_path_of_collection(c) for c in MULTI_STAR_COLLECTIONS]
+    paths_all = paths_tags + paths_colls
 
-#     print(f"EB subject ids of page [{page_start}, {page_end_inclusive}] ; end_subject_id_exclusive={end_subject_id_exclusive}")
-#     get_subject_ids_of_tag(
-#         "eclipsingbinary",
-#         page_start,
-#         page_end_inclusive,
-#         end_subject_id_exclusive=end_subject_id_exclusive,
-#         page_result_func=save_subject_ids_of_page,
-#     )
+    all_ids = np.array([], dtype=int)
+    for a_path in paths_all:
+        a_file_ids = np.genfromtxt(a_path, dtype=int)
+        all_ids = np.concatenate((all_ids, a_file_ids), 0)
+    all_ids = np.unique(all_ids)
+
+    with open(out_path, "w") as f:  # always overwrite
+        np.savetxt(f, all_ids, fmt="%s")
+
+
+def save_all_subject_ids_from_all(skip_if_exists=True):
+    save_all_subject_ids_from_tags(skip_if_exists=skip_if_exists)
+    save_all_subject_ids_from_collections(skip_if_exists=skip_if_exists)
+    _combine_n_save_all_subject_ids()
+
+
+#
+# Top level driver
+#
+if __name__ == "__main__":
+    save_all_subject_ids_from_all(skip_if_exists=True)
